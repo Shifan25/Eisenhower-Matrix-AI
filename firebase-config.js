b7b09d0c560c7d1732d1ab7a30baba482e35a87b
Shifan25 <shifanishak@gmail.com>
2025-07-25 12:42:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// firebase-config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyDIaC6hiNxTL2RlmprzQFzhcfKR7R-_HH0",
   authDomain: "eisenhower-matrix-f4901.firebaseapp.com",
   projectId: "eisenhower-matrix-f4901",
-  storageBucket: "eisenhower-matrix-f4901.firebasestorage.app",
+  storageBucket: "eisenhower-matrix-f4901.appspot.com",   // <-- If console shows appspot.com use this
   messagingSenderId: "700179285214",
   appId: "1:700179285214:web:bcf86cb058a7fbf268ae4f"
 };
