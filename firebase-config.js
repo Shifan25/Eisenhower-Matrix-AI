ad4fcff8764d9e6e553de5b7d6d98dc5d1d328e3
Shifan25 <shifanishak@gmail.com>
2025-07-25 11:11:12 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "yourproject.firebaseapp.com",
-  projectId: "yourproject",
-  storageBucket: "yourproject.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+const firebaseConfig = {
+  apiKey: "AIzaSyDIaC6hiNxTL2RlmprzQFzhcfKR7R-_HH0",
+  authDomain: "eisenhower-matrix-f4901.firebaseapp.com",
+  projectId: "eisenhower-matrix-f4901",
+  storageBucket: "eisenhower-matrix-f4901.firebasestorage.app",
+  messagingSenderId: "700179285214",
+  appId: "1:700179285214:web:bcf86cb058a7fbf268ae4f"
 };
